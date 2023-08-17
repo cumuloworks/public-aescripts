c9f3c747ba106d026fb3d502cec987fcbe5c1f0b
cumuloworks <mail@cumulo.works>
2023-08-17 09:36:52 +0900

    アルゴリズム高速化。

--- (projects)/node_system_line.jsx
+++ (projects)/node_system_line.jsx
@@ -1,4 +1,5 @@
 (function () {
+  // GUI部分
   var mainWindow = new Window("palette", "Script Settings", undefined);
   mainWindow.orientation = "column";
 
@@ -35,155 +36,120 @@
   mainWindow.center();
   mainWindow.show();
 
-  runButton.onClick = function () {
-    if (app.project.file) {
-      app.project.save(app.project.file);
-    } else {
-      alert("Project must be saved first manually!");
-      return;
-    }
+  runButton.onClick = mainFunction;
 
-    app.beginUndoGroup("Script Execution");
-
+  function mainFunction() {
+    app.beginUndoGroup("Start Generating Lines");
+    app.disableRendering = true;
     var duplicates = parseInt(duplicatesInput.text);
     var maxTries = parseInt(maxTriesInput.text);
     var minDist = parseInt(minDistInput.text);
     var maxDist = parseInt(maxDistInput.text);
 
-    processedLayers = [];
-    processLayers(duplicates, maxTries, minDist, maxDist);
-
-    app.endUndoGroup();
-  };
-
-  var processedLayers = []; // Add this line to create a new array for storing processed layers
-  var pairs = [];
-
-  function processLayers(duplicates, maxTries, minDist, maxDist) {
-    function processLayer(layer) {
-      // Check if the layer has "NODE Line" effect
-      var effect = layer.effect("NODE Line");
-      if (!effect) {
-        alert("This is not a NODE Line layer.");
-        return;
-      }
-
-      // Get the control layer index
-      var controlLayerIndex = -1;
-      for (var i = 1; i <= app.project.activeItem.numLayers; i++) {
-        if (
-          app.project.activeItem.layer(i).name === "【CONTROL】 TEXTS BELOW ↓↓↓"
-        ) {
-          controlLayerIndex = i;
-          break;
-        }
-      }
-
-      if (controlLayerIndex == -1) {
-        alert("Control Layer not found.");
-        return;
-      }
-
-      // Get layers below control layer
-      var belowLayers = [];
-      for (
-        var i = controlLayerIndex + 1;
-        i <= app.project.activeItem.numLayers;
-        i++
-      ) {
-        belowLayers.push(app.project.activeItem.layer(i));
-      }
-
-      if (belowLayers.length < 2) {
-        alert("Not enough layers below the control layer.");
-        return;
-      }
-
-      if (processedLayers.length >= belowLayers.length) {
-        processedLayers = [];
-      }
-
-      // Select the first layer randomly
-      var originLayer;
-      do {
-        originLayer =
-          belowLayers[Math.floor(Math.random() * belowLayers.length)];
-      } while (processedLayers.indexOf(originLayer.index) !== -1); // Keep selecting until an unprocessed layer is found
-
-      // Set the first parameter "Origin" of "NODE Line" effect
-      effect.property(1).setValue(originLayer.index);
-      processedLayers.push(originLayer.index);
-
-      // Select the second layer with distance between minDist and maxDist to the first layer
-      var destinationLayer = null;
-      for (var i = 0; i < maxTries; i++) {
-        var tempLayer;
-        do {
-          tempLayer =
-            belowLayers[Math.floor(Math.random() * belowLayers.length)];
-        } while (
-          processedLayers.indexOf(tempLayer.index) !== -1 ||
-          tempLayer.index === originLayer.index
-        ); // Keep selecting until an unprocessed layer is found
-
-        // Check if the distance is between minDist and maxDist
-        var distance = Math.sqrt(
-          Math.pow(
-            originLayer.position.value[0] - tempLayer.position.value[0],
-            2
-          ) +
-            Math.pow(
-              originLayer.position.value[1] - tempLayer.position.value[1],
-              2
-            )
-        );
-        if (minDist <= distance && distance <= maxDist) {
-          destinationLayer = tempLayer;
-          break;
-        }
-      }
-
-      if (!destinationLayer) {
-        layer.remove();
-        return;
-      }
-
-      var pair = [originLayer.index, destinationLayer.index];
-      pair.sort(function (a, b) {
-        return a - b;
-      });
-
-      // 重複ペアの確認
-      for (var i = 0; i < pairs.length; i++) {
-        if (pairs[i][0] === pair[0] && pairs[i][1] === pair[1]) {
-          // 重複ペアが見つかったらレイヤーを削除
-          layer.remove();
-          return;
-        }
-      }
-
-      // ペアを記録
-      pairs.push(pair);
-
-      // NODE Lineエフェクトの設定
-      effect.property(1).setValue(originLayer.index);
-      effect.property(2).setValue(destinationLayer.index);
-      processedLayers.push(originLayer.index);
-      processedLayers.push(destinationLayer.index);
+    var controlLayerIndex = getControlLayerIndex();
+    if (controlLayerIndex == -1) {
+      alert("Control Layer not found.");
+      return;
     }
-    // Select the first layer
-    processLayer(app.project.activeItem.selectedLayers[0]);
+    var belowLayersCoordinates = getBelowLayersCoordinates(controlLayerIndex);
+    var pairs = createPairs(belowLayersCoordinates, minDist, maxDist, maxTries);
 
-    // Duplicate the layer and process the duplicates
-    for (var i = 0; i < duplicates; i++) {
-      var duplicateLayer = app.project.activeItem.selectedLayers[0].duplicate();
+    for (var i = 0; i < pairs.length; i++) {
+      pairs[i][0] += pairs.length;
+      pairs[i][1] += pairs.length;
+    }
 
-      // Increment all processedLayers indices before processing the duplicated layer
-      for (var j = 0; j < processedLayers.length; j++) {
-        processedLayers[j]++;
+    // ペアのデバッグ出力
+    var userConfirmed = confirm("Created pairs: " + JSON.stringify(pairs));
+    if (userConfirmed) {
+      // continue with the rest of the code
+    } else {
+      app.disableRendering = false;
+      return; // exit the function or loop
+    }
+
+    // 複製するレイヤー数だけオリジナルレイヤーを複製
+    var originalLayer = app.project.activeItem.selectedLayers[0];
+    var duplicateLayers = [];
+    for (var i = 0; i < pairs.length; i++) {
+      duplicateLayers.push(originalLayer.duplicate());
+    }
+
+    // オリジナルレイヤーのインデックス
+    var originalIndex = originalLayer.index;
+
+    // 複製したレイヤーに設定を適用
+    for (var index = 0; index < pairs.length; index++) {
+      var pair = pairs[index];
+      var duplicateLayer = duplicateLayers[index];
+
+      var effect = duplicateLayer.effect("NODE Line");
+      if (effect) {
+        effect.property(1).setValue(pair[0]);
+        effect.property(2).setValue(pair[1]);
+      } else {
+        alert(
+          "Duplicate layer" +
+            duplicateLayer.index +
+            "does not have the NODE Line effect."
+        );
       }
-
-      processLayer(duplicateLayer);
     }
   }
+
+  function getControlLayerIndex() {
+    var controlLayerIndex = -1;
+    for (var i = 1; i <= app.project.activeItem.numLayers; i++) {
+      if (
+        app.project.activeItem.layer(i).name === "【CONTROL】 TEXTS BELOW ↓↓↓"
+      ) {
+        controlLayerIndex = i;
+        break;
+      }
+    }
+    return controlLayerIndex; // この行を追加
+  }
+
+  function getBelowLayersCoordinates(controlLayerIndex) {
+    var coordinates = [];
+    for (
+      var i = controlLayerIndex + 1;
+      i <= app.project.activeItem.numLayers;
+      i++
+    ) {
+      var layer = app.project.activeItem.layer(i);
+      coordinates.push({
+        index: i,
+        position: layer.position.value,
+      });
+    }
+    return coordinates;
+  }
+
+  function createPairs(belowLayersCoordinates, minDist, maxDist, maxTries) {
+    var pairs = [];
+    for (var i = 0; i < belowLayersCoordinates.length; i++) {
+      var origin = belowLayersCoordinates[i];
+      for (var tries = 0; tries < maxTries; tries++) {
+        var destinationIndex = Math.floor(
+          Math.random() * belowLayersCoordinates.length
+        );
+        if (destinationIndex === i) continue; // Skip if selected itself
+        var destination = belowLayersCoordinates[destinationIndex];
+
+        var distance = Math.sqrt(
+          Math.pow(origin.position[0] - destination.position[0], 2) +
+            Math.pow(origin.position[1] - destination.position[1], 2)
+        );
+        if (minDist <= distance && distance <= maxDist) {
+          pairs.push([origin.index, destination.index]);
+          break;
+        }
+      }
+    }
+    return pairs;
+  }
+  app.disableRendering = false;
+  app.endUndoGroup();
+  alert("Finished.")
 })();
